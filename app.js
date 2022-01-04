52db1bdda9825b02cf725db6c248c39be56f7ebc
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-01-04 14:14:00 -0800

    Complete Tic-Tac-Toe Bare Minimum Requirements

--- app.js
+++ app.js
@@ -2,45 +2,48 @@ window.addEventListener('DOMContentLoaded', (event) => {
   // console.log('DOM fully loaded and parsed');
     document.querySelectorAll('td').forEach(cell => cell.addEventListener('click', cellClickHandler));
     document.getElementById('restartButton').addEventListener('click', restartButtonHandler);
-
-  // console.log('document', document)
 });
 
-var nextPieceIsX = true;
-var stateOfBoard = ['', '', '', '', '', '', '', '', ''];
-var gameOver = false;
-// var cellClickActive = true;
+var state = {
+  nextPieceIsX: true,
+  stateOfBoard: ['', '', '', '', '', '', '', '', ''],
+  gameOver: false,
+}
+
+
+var header2 = document.querySelector('h2');
+var header3 = document.querySelector('h3');
+
 
 function cellClickHandler(element) {
-  // console.log('elementID', element.srcElement.id)
-  // console.log('Cell clicked');
-
   var elementID = element.srcElement.id;
-  if (document.getElementById(elementID).innerHTML !== '') {
-    console.log('CELL TAKEN! Try Again!')
-  } else if (nextPieceIsX) {
-    document.getElementById(elementID).innerHTML = 'X';
-    var elementIndex = parseInt(elementID);
-    stateOfBoard[elementIndex] = 'X';
-    nextPieceIsX = !nextPieceIsX;
-    console.log('state of board', stateOfBoard);
 
+  if (state.stateOfBoard[elementID] !== '') {
+    header3.innerText = 'Cell taken. Try again.';
+    return;
   } else {
-    document.getElementById(elementID).innerHTML = 'O';
-    var elementIndex = parseInt(elementID);
-    stateOfBoard[elementIndex] = 'O';
-    nextPieceIsX = !nextPieceIsX;
-    console.log('state of board', stateOfBoard);
-
+    placeXorO(elementID);
   }
-
   checkIfWinner(checkIfBoardFull);
-
 };
 
-function checkIfWinner(callback) {
-  // var table = document.getElementById('TttTable');
 
+function placeXorO(elementID) {
+  var elementIndex = parseInt(elementID);
+
+  if (state.nextPieceIsX) {
+    document.getElementById(elementID).innerHTML = 'X';
+    state.stateOfBoard[elementIndex] = 'X';
+  } else {
+    document.getElementById(elementID).innerHTML = 'O';
+    state.stateOfBoard[elementIndex] = 'O';
+  }
+  state.nextPieceIsX = !state.nextPieceIsX;
+  header3.innerText = 'Play allowed.';
+};
+
+
+function checkIfWinner(callback) {
   var winningCombos = [
     [1, 2, 3],
     [4, 5, 6],
@@ -56,66 +59,51 @@ function checkIfWinner(callback) {
     var currentCombo = winningCombos[i];
     var index1 = currentCombo[0];
     var index2 = currentCombo[1];
-
     var index3 = currentCombo[2];
 
-    var first = stateOfBoard[index1 - 1];
-    var second = stateOfBoard[index2 - 1];
-    var third = stateOfBoard[index3 - 1];
+    var first = state.stateOfBoard[index1 - 1];
+    var second = state.stateOfBoard[index2 - 1];
+    var third = state.stateOfBoard[index3 - 1];
 
     if (first === '' || second === '' || third === '') {
 
     } else if (first === second && second === third) {
-      console.log(first + ' WON!');
       var header = document.querySelector('h2');
       header.innerText = first + ' WON!';
-      cellClickActive = false;
       return handleGameOver();
     }
-
   }
   callback();
-  // return false;
 };
 
+
 function checkIfBoardFull() {
-
   var full = true;
-  var table = document.getElementById('TttTable');
 
-  for (var row of table.rows) {
-    for (var cell of row.cells) {
-      if (cell.innerHTML === '') {
-        full = false;
-      }
+  for (var i = 0; i < state.stateOfBoard.length; i++) {
+    if (state.stateOfBoard[i] === '') {
+      full = false;
     }
   }
 
   if (full) {
-    var header = document.querySelector('h2');
-    header.innerText = 'Board is full. Start a new game.';
-    cellClickActive = false;
+    header2.innerText = 'Board is full. Start a new game.';
     handleGameOver();
   }
-
 };
 
 
 function handleGameOver() {
-  // if (!cellClickActive) {
     document.querySelectorAll('td').forEach(cell => cell.removeEventListener('click', cellClickHandler));
-  // }
 };
 
-function restartButtonHandler() {
-  nextPieceIsX = true;
-  stateOfBoard = ['', '', '', '', '', '', '', '', ''];
-  gameOver = false;
-  cellClickActive = true;
-  document.querySelectorAll('td').forEach(cell => cell.addEventListener('click', cellClickHandler));
-  var header = document.querySelector('h2');
-  header.innerText = '';
 
+function restartButtonHandler() {
+  state.nextPieceIsX = true;
+  state.stateOfBoard = ['', '', '', '', '', '', '', '', ''];
+  state.gameOver = false;
+  document.querySelectorAll('td').forEach(cell => cell.addEventListener('click', cellClickHandler));
+  header2.innerText = '';
 
   var table = document.getElementById('TttTable');
 
@@ -124,7 +112,51 @@ function restartButtonHandler() {
       cell.innerHTML = '';
     }
   }
-
 }
 
+
+
+
+
+
+
+
+
+
+
+
 // document.querySelectorAll('td').forEach(cell => cell.addEventListener('click', cellClickHandler));
+
+
+
+  // Before refactoring for separation of concerns in cellClickHandler
+  // if (document.getElementById(elementID).innerHTML !== '') {
+  //   console.log('CELL TAKEN! Try Again!')
+  // } else if (nextPieceIsX) {
+  //   document.getElementById(elementID).innerHTML = 'X';
+  //   var elementIndex = parseInt(elementID);
+  //   stateOfBoard[elementIndex] = 'X';
+  //   nextPieceIsX = !nextPieceIsX;
+  //   console.log('state of board', stateOfBoard);
+
+  // } else {
+  //   document.getElementById(elementID).innerHTML = 'O';
+  //   var elementIndex = parseInt(elementID);
+  //   stateOfBoard[elementIndex] = 'O';
+  //   nextPieceIsX = !nextPieceIsX;
+  //   console.log('state of board', stateOfBoard);
+  // }
+
+
+
+
+// checkIfBoardFull before refactoring for separation of concerns
+    // var table = document.getElementById('TttTable');
+
+  // for (var row of table.rows) {
+  //   for (var cell of row.cells) {
+  //     if (cell.innerHTML === '') {
+  //       full = false;
+  //     }
+  //   }
+  // }
